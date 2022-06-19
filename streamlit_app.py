4827d79d07021bc258764a81eb88f6d70464c93f
shopcaptor <107816976+shopcaptor@users.noreply.github.com>
2022-06-19 16:14:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 
 import streamlit
+import pandas
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 
 streamlit.title('Hey')
+streamlit.datafram(my_fruit_list)
