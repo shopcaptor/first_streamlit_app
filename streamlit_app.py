48e4797ac7547b35954fc1a3b21d4cc471319eff
shopcaptor <107816976+shopcaptor@users.noreply.github.com>
2022-06-19 16:23:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('Hey')
 
 
-fruits_selected = streamlit.multiselect("pick:", list(my_fruit_list.index))
-fruits_to_show = my_fruits_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("pick:", list(my_fruit_list.index), ['Avocado'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
